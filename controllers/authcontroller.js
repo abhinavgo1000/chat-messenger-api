e8104af548e832d4c5a18fa20d903fb7adceac49
Abhinav Goel <abhinavgo1000@gmail.com>
2024-04-27 09:38:18 -0400

    email service placeholder added and authentication enhanced

--- controllers/authcontroller.js
+++ controllers/authcontroller.js
@@ -1,6 +1,15 @@
 const bcrypt = require('bcryptjs');
+const nodemailer = require('nodemailer');
+const sendgridTransport = require('nodemailer-sendgrid-transport');
+const crypto = require('crypto');
 
 const User = require('../models/user');
+
+const transporter = nodemailer.createTransport(sendgridTransport({
+    auth: {
+        // api_key:
+    }
+}));
 
 exports.getLogin = (req, res) => {
     res.render('', {
@@ -63,6 +72,14 @@ exports.postSignup = (req, res) => {
         });
         return user.save();
     })
+    .then(() => {
+        return transporter.sendMail({
+            to: email,
+            from: 'no-reply@abhinavgoel.org',
+            subject: 'Congrats! Yoyr account is created',
+            html: '<h1>You successfully signed up!</h1>'
+        });
+    })
     .catch((err) => {
         console.log(err);
     });
@@ -70,6 +87,60 @@ exports.postSignup = (req, res) => {
 
 exports.postLogout = (req, res) => {
     req.session.destroy((err) => {
+        console.log(err);
+    });
+};
+
+exports.postReset = (req, res) => {
+    crypto.randomBytes(32, (err, buffer) => {
+        if (err) {
+            console.log(err);
+            res.redirect('');
+        }
+        const token = buffer.toString('hex');
+        User.findOne({email: req.body.email})
+        .then((user) => {
+            user.resetToken = token;
+            user.resetTokenExpiration = Date.now() + 3600000;
+            return user.save();
+        })
+        .then(() => {
+            transporter.sendMail({
+                to: req.body.email,
+                from: 'no-reply@abhinavgoel.org',
+                subject: 'Password reset',
+                html: `
+                    <p>You recently requested password reset</p>
+                    <p>Click on this <a href="http://localhost:4200/reset-password/${token}">link</a> to reset your password</p>`
+            });
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+    });
+};
+
+exports.postNewPassword = (req, res) => {
+    const newPassword = req.body.password;
+    const userId = req.body.userId;
+    const passwordToken = req.body.passwordToken;
+    let resetUser;
+    User.findOne({
+        resetToken: passwordToken, 
+        resetTokenExpiration: {$gt: Date.now()}, 
+        _id: userId
+    })
+    .then((user) => {
+        resetUser = user;
+        return bcrypt.hash(newPassword, 12);
+    })
+    .then((hashedPassword) => {
+        resetUser.password = hashedPassword;
+        resetUser.resetToken = null;
+        resetUser.resetTokenExpiration = null;
+        return resetUser.save();
+    })
+    .catch((err) => {
         console.log(err);
     });
 };
